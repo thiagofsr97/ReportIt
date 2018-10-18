393e3d1d43a352bcf0ee798bc4d4e3cdefe7654a
edsonapf <edsonalves@cc.ci.ufpb.br>
2018-10-18 15:05:06 -0300

    Report updated

--- src/business/control/Report.java
+++ src/business/control/Report.java
@@ -4,15 +4,18 @@ import business.model.Occurrence;
 
 public abstract class Report {
     
-    protected Occurrence occurrence;
+    protected Occurrence _occurrence;
+    private OccurrenceManager _occManager;
     
-    public final void generateReport(int idOccurrence){
+    
+    public final void generateReport(OccurrenceManager occMan,int idOccurrence){
+        this._occManager = occMan;
         getOccurrenceData(idOccurrence);
         reportFileType();
     }
     
     protected void getOccurrenceData(int idOccurrence){
-        occurrence = OccurrenceManager.getOcorrencia(idOccurrence);
+        _occurrence = this._occManager.getOccurrence(idOccurrence);
         System.out.println("Obteve os dados da Occorrência.");
     }
     
