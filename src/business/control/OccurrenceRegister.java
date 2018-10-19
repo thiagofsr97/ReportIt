468bd40d10f8c9c1e21dc9a61bcd5e81464907fa
João Vinicius <joao.vinicius2008@hotmail.com>
2018-10-18 23:46:37 -0300

    Corrigindo commit

--- src/business/control/OccurrenceRegister.java
+++ src/business/control/OccurrenceRegister.java
@@ -1,17 +1,30 @@
 package business.control;
 
-
 import business.model.Occurrence;
 
 public class OccurrenceRegister{
-
+    
     private OccurrenceManager occurrenceManager;
+    private static OccurrenceRegister occurrenceRegister = null;
+    private OccurrenceRegister(){
+        this.occurrenceManager = OccurrenceManager.getInstance();
+    }
+     /*
+     *
+     * Applying Singleton Design Pattern
+     *
+     */
+     public static OccurrenceRegister getInstance(){
+        if(occurrenceRegister == null){
+            occurrenceRegister = new OccurrenceRegister();
+        }
+        return occurrenceRegister;
+     }
     OccurrenceRegister(OccurrenceManager occurrenceManager){
         this.occurrenceManager = occurrenceManager;
     }
-
-    public void register(Occurrence occurrence){
+     public void register(Occurrence occurrence){
         this.occurrenceManager.insertOcurrence(occurrence);
+     }
 
-    }
 }
